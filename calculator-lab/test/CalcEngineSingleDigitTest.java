5b7a5a2a69b85121eba1e19698f0de21159d65ad
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-01-24 18:42:22 +0000

    RPN Single Digit with Parenthesis working

--- calculator-lab/test/CalcEngineSingleDigitTest.java
+++ calculator-lab/test/CalcEngineSingleDigitTest.java
@@ -18,14 +18,23 @@ public class CalcEngineSingleDigitTest
   public void testRPNWithoutParenthesis()
   {
 	String infix = "3 * 4 + 6";
-	assertEquals("34*6+", toPostfix(infix));
+	assertEquals("34*6+", toPostfixSimple(infix));
 	
 	String infix2 = "3 - 4 / 6";
-	assertEquals("346/-", toPostfix(infix2));
+	assertEquals("346/-", toPostfixSimple(infix2));
   }
 
+  @Test
+  public void testRPNWithParenthesis()
+  {
+	String infix = "3 * (4 + 6)";
+	assertEquals("346+*", toPostfixModerate(infix));
+	
+	String infix2 = "((1 + 2) * 3 - 4) * 5";
+	assertEquals("12+3*4-5*", toPostfixModerate(infix2));
+  }
 
-  public String toPostfix(String infix)
+  public String toPostfixSimple(String infix)
   {
 	String postfix = "";
 	Character currentChar;
@@ -70,6 +79,72 @@ public class CalcEngineSingleDigitTest
 	return postfix;
   }
 
+
+  public String toPostfixModerate(String infix)
+  {
+	String postfix = "";
+	Character currentChar;
+
+	infix = infix.replaceAll("\\s+",""); //removing whitespace
+	//	System.out.println(infix);
+	for (int i=0; i<infix.length(); i++)
+	{
+	  //	  System.out.println(i);
+	  currentChar = infix.charAt(i);
+	  //	  System.out.println(currentChar);
+	  if (Character.isDigit(currentChar))
+	  {
+		postfix += Character.toString(currentChar);
+	  }
+	  else if (currentChar.equals('('))
+	  {
+		operatorStack.push(currentChar);
+	  }
+	  else if (currentChar.equals(')'))
+	  {
+		boolean openParen = false;
+		
+		while ( (!operatorStack.empty()) && (openParen == false))
+		{
+		  if (operatorStack.peek() != '(') 
+		  {
+			postfix += Character.toString(operatorStack.pop()); //Pop all operands until reach an open parenthesis
+		  }
+		  else
+		  {
+			operatorStack.pop(); //Pop the first open parenthesis seen
+			openParen = true;
+		  }
+		}
+	  }
+	  else if ( (currentChar.equals('+')) || (currentChar.equals('-'))
+		  ||	(currentChar.equals('*')) || (currentChar.equals('/')) )
+	  {
+		boolean noPreced = false;
+
+		while ( (!operatorStack.empty()) && (noPreced == false) )
+		{		   
+		  if (precedenceCheck(currentChar, operatorStack.peek()) < 0)
+		  {
+			postfix += Character.toString(operatorStack.pop());
+		  } 
+		  else
+		  {
+			noPreced = true;
+		  }
+		}
+		operatorStack.push(currentChar);
+
+	  }	  
+
+	}
+	while (!operatorStack.empty())
+	{
+	  postfix += Character.toString(operatorStack.pop());
+	}
+
+	return postfix;
+  }
 
   public int precedenceCheck(char current, char topOfStack)
   {
