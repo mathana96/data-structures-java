3146ee35b204a6b5a6b598695311e60f3cfe7722
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-02-08 23:40:20 +0000

    Changed priority queue to arrayList. Modified ParseData class and test. Added more to skeleton

---
+++ eng-spanish-dict/src/utils/ParseData.java
@@ -0,0 +1,75 @@
+package utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import models.Pair;
+
+public class ParseData
+{
+	ArrayList<Pair> pairs = new ArrayList<>();
+//	String dictPath = "././data/spa-to-eng.txt";
+	
+	public ArrayList<Pair> readFile(String path) throws Exception
+	{
+		Scanner rawPairs = null;
+		try
+		{
+			File file = new File(path);
+			rawPairs = new Scanner(file);
+			
+			
+		} 
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		while (rawPairs.hasNextLine())
+		{
+			String[] tokens = rawPairs.nextLine().replaceAll("\uFFFD", "").trim().split("\\t");
+			
+			if (tokens.length == 2)
+			{
+				Pair pair = new Pair(tokens[0], tokens[1]);
+				pairs.add(pair);
+				siftUp();
+			}
+			else
+			{
+	      rawPairs.close();
+	      throw new Exception("Invalid token length: "+ tokens.length);
+			}
+			
+		}
+		rawPairs.close();	
+		return pairs;
+	}
+	
+	public void siftUp()
+	{
+		
+		int k = pairs.size() - 1;
+		
+		while (k > 0)
+		{
+//			System.out.println(numbers);
+
+			int p = (k-1)/2;
+			Pair child = pairs.get(k);
+			Pair parent = pairs.get(p);
+			
+			if (child.compareTo(parent) > 0)
+			{
+				pairs.set(p, child);
+				pairs.set(k, parent);
+				
+				k = p;
+			}
+			else
+				break;	
+		}
+	}
+
+}
--- eng-spanish-dict/test/ParserTest.java
+++ eng-spanish-dict/test/ParserTest.java
@@ -1,59 +1,25 @@
 import static org.junit.Assert.*;
 
-import java.io.File;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
 import models.Pair;
+import utils.ParseData;
 
 public class ParserTest
 {
 	
-	String dictPath = "././data/spa-to-eng.txt";
-	PriorityQueue<Pair> pairs = new PriorityQueue<>();
+	String path = "././data/spa-to-eng.txt";
+	ArrayList<Pair> pairs = new ArrayList<>();
 
 	@Test
 	public void test() throws Exception
 	{
-		readFile(dictPath);
+		ParseData parseData = new ParseData();
+		pairs = parseData.readFile(path);
 		System.out.println(pairs);	
 //		System.out.println(pairs.peek());
 	}
 
-	
-	public void readFile(String path) throws Exception
-	{
-		Scanner rawPairs = null;
-		try
-		{
-			File file = new File(path);
-			rawPairs = new Scanner(file);
-			
-			
-		} 
-		catch (Exception e)
-		{
-			System.out.println(e);
-		}
-		
-		while (rawPairs.hasNextLine())
-		{
-			String[] tokens = rawPairs.nextLine().replaceAll("\uFFFD", "").trim().split("\\t");
-			
-			if (tokens.length == 2)
-			{
-				Pair pair = new Pair(tokens[0], tokens[1]);
-				pairs.add(pair);
-			}
-			else
-			{
-	      rawPairs.close();
-	      throw new Exception("Invalid token length: "+ tokens.length);
-			}
-			
-		}
-		rawPairs.close();	
-	}
 }
