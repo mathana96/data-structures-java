2f315674fd83b76da5b3eacea586d82a5632fd02
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-02-27 19:45:00 +0000

    Changed implementation of Node class. Added pretty print

--- binary-search-trees/src/BinarySearchTree.java
+++ binary-search-trees/src/BinarySearchTree.java
@@ -1,26 +1,110 @@
+import java.util.Stack;
 
 public class BinarySearchTree
 {
-	TreeNode root;
+	public Node root;
+	/**
+	 * 
+	 * Inner class. Some guidelines here
+	 * http://stackoverflow.com/questions/18396016/when-to-use-inner-classes-in-java-for-helper-classes
+	 *
+	 */
+	private class Node
+	{
+		private int data;
+		private Node left;
+		private Node right;
+		
+		private Node(int data)
+		{
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	
 	
 	public BinarySearchTree()
 	{
 		this.root = null;
 	}
 	
-	public TreeNode insert(TreeNode node, int num) //Given a node pointer, insert num recursively
+	/**
+	 * Public client method used for recursive calls to private helper methods
+	 * 
+	 */
+	public void insert(int data)
+	{
+		root = insert(root, data);
+	}
+	
+	/**
+	 * Private helper method. Recursive insert
+	 * @param node the node
+	 * @param data the data of the node
+	 */
+	private Node insert(Node node, int data) //Given a pointer to a node, insert data
 	{
 		if (node == null)
-			node = new TreeNode(num);
+			node = new Node(data); //Create a new node if null at where pointer is
 		else
 		{
-			if (num < node.number)
-				node.leftChild = insert(node.leftChild, num);
-			else
-				node.rightChild = insert(node.rightChild, num);
-				
+			if (data < node.data)
+				node.left = insert(node.left, data); //Put whatever returned by the call into the left child
+			else if (data > node.data)
+				node.right = insert(node.right, data);
+			else // if (data is the same as the node's data), replace it 
+				node.data = data;
 		}	
 		return node;
 	}
+	
+	/**
+	 * Class taken from;
+	 * https://github.com/camluca/Samples/blob/master/Tree.java
+	 */
+	public void displayTree()
+	{
+		Node r = root;
+		Stack<Node> globalStack = new Stack<>();
+		globalStack.push(r);	
+		int emptyLeaf = 32;
+		boolean isRowEmpty = false;
+		System.out.println("****......................................................****");
+		while(isRowEmpty==false)
+		{
+
+			Stack<Node> localStack = new Stack<>();
+			isRowEmpty = true;
+			for(int j=0; j<emptyLeaf; j++)
+				System.out.print(' ');
+			while(globalStack.isEmpty()==false)
+			{
+				Node temp = globalStack.pop();
+				if(temp != null)
+				{
+					System.out.print(temp.data);
+					localStack.push(temp.left);
+					localStack.push(temp.right);
+					if(temp.left != null ||temp.right != null)
+						isRowEmpty = false;
+				}
+				else
+				{
+					System.out.print("--");
+					localStack.push(null);
+					localStack.push(null);
+				}
+				for(int j=0; j<emptyLeaf*2-2; j++)
+					System.out.print(' ');
+			}
+			System.out.println();
+			emptyLeaf /= 2;
+			while(localStack.isEmpty()==false)
+				globalStack.push( localStack.pop() );
+		}
+	System.out.println("****......................................................****");
+	} 
+
 	
 }
--- binary-search-trees/test/TestInsert.java
+++ binary-search-trees/test/TestInsert.java
@@ -1,20 +1,27 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 import org.junit.Test;
 
 public class TestInsert
 {
 
+	BinarySearchTree tree = new BinarySearchTree();
+	
 	@Test
 	public void test()
-	{
-		BinarySearchTree tree = new BinarySearchTree();
-		int[] numbers = {3,76,5,32,2,8,4,9,23,6};
+	{	
+		int[] numbers = {23,76,5,32,2,8,4,9,6};
 		for (int num : numbers)
 		{
-			tree.insert(tree.root, num);
+			
+			tree.insert(num);
 		}
 		
+		tree.displayTree();
+		
 	}
-
+	
 }
