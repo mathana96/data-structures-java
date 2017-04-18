607f77d104e5a88bb7323e35892f3dc006e58bf0
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-04-18 19:49:00 +0100

    Able to search siblings

--- happy-family-tree/src/controllers/FamilyDriver.java
+++ happy-family-tree/src/controllers/FamilyDriver.java
@@ -49,64 +49,92 @@ public class FamilyDriver
 		String victim = input.nextLine().toLowerCase();
 		victim = victim.substring(0, 1).toUpperCase() + victim.substring(1);
 
-		System.out.println("\nExcellent... Now read the following carefully as these are precise spells which only work if you say them right!");
-
-		System.out.println("\n\n***************Spell Guide (Note: Grammar is still evolving)***************");
-
-		System.out.println("\nThe 'who' spell. Chant 'who' followed by one these words.");
-		System.out.println("(mother, father, siblings, cousins, spouse, grandparents, uncles, aunties");
-
-		String answer = input.nextLine();
-		String[] ansTokens = answer.split("\\s");
-		if (ansTokens.length == 2)
-		{
-			String key = ansTokens[0].toLowerCase();
-			String val = ansTokens[1].toLowerCase();
-			noseySwitch(victim, key, val);
-		}
-		else
-		{
-			System.out.println("Unruly peasant! The spell didn't work HA!");
-		}
-
-	}
-
-	public void noseySwitch(String victim, String key, String val)
-	{
 		if (people.containsKey(victim))
 		{
-			switch(key)
-			{
-				case "who":
-					switch (val)
-					{
-						case "mother":
-							getMother(victim);
-							break;
-							
-						case "father":
-							getFather(victim);
-							break;
-						
-						default:
-							System.out.println("Look what you've done! I warned that this is a deliate process! Ugh");
-					}
-				
-				default:
-					System.out.println("Look what you've done! I warned that this is a deliate process! Ugh");
 
+			System.out.println("\nExcellent... Now read the following carefully as these are precise spells which only work if you say them right!");
+
+			System.out.println("\n\n***************Spell Guide (Note: Grammar is still evolving)***************");
+
+			System.out.println("\nThe 'who' spell. Chant 'who' followed by one these words.");
+			System.out.println("(mother, father, siblings, cousins, spouse, grandparents, uncles, aunties");
+
+			String answer = input.nextLine();
+			String[] ansTokens = answer.split("\\s");
+			if (ansTokens.length == 2)
+			{
+				String key = ansTokens[0].toLowerCase();
+				String val = ansTokens[1].toLowerCase();
+				noseySwitch(victim, key, val);
 			}
+			else
+			{
+				System.out.println("Unruly peasant! The spell didn't work HA!");
+			}
+
 		}
 		else
 		{
 			System.out.println("This soul belongs to a different kingdom");
 		}
+	}
+
+	public void noseySwitch(String victim, String key, String val)
+	{
+
+		switch(key)
+		{
+			case "who":
+				switch (val)
+				{
+					case "mother":
+						getMother(victim);
+						break;
+
+					case "father":
+						getFather(victim);
+						break;
+
+					case "spouse":
+						getSpouse(victim);
+						break;
+
+					case "siblings":
+						getSiblings(victim);
+						break;
+		
+					case "cousins":
+						getFather(victim);
+						break;
+		
+					case "uncles":
+						getFather(victim);
+						break;
+		
+					case "aunties":
+						getFather(victim);
+						break;
+		
+					case "grandparents":
+						getFather(victim);
+						break;
+
+					default:
+						System.out.println("Look what you've done! I warned you that this is a deliate process! Ugh");
+				}
+				break;
+
+			default:
+				System.out.println("Look what you've done! I warned you that this is a deliate process! Ugh");
+
+		}
+
 
 	}
 	public void getMother(String v)
 	{
 		Person victim = people.get(v);
-		
+
 		if (victim.mother != null)
 		{
 			String name = victim.mother.name;
@@ -122,7 +150,7 @@ public class FamilyDriver
 	public void getFather(String v)
 	{
 		Person victim = people.get(v);
-		
+
 		if (victim.father != null)
 		{
 			String name = victim.father.name;
@@ -132,6 +160,61 @@ public class FamilyDriver
 		else
 		{
 			System.out.println("Doesn't have a father, the poor .....");
+		}
+	}
+
+	public void getSpouse(String v)
+	{
+		Person victim = people.get(v);
+
+		if (victim.spouse != null)
+		{
+			String name = victim.spouse.name;
+			Person spouse = people.get(name);
+			System.out.println(spouse.name + " spouse of " + v + ". Married on god knows when.");
+		}
+		else
+		{
+			System.out.println("Doesn't have a spouse, the poor .....");
+		}
+	}
+	
+	public void getSiblings(String v)
+	{
+		Person victim = people.get(v);
+
+		if ( (victim.father != null) || (victim.mother != null) )
+		{
+
+			if (victim.father != null) 
+			{
+				String name = victim.father.name;
+				Person father = people.get(name);
+				List<Person> children = father.children;
+				System.out.println("Siblings of " + v + ". If none found, none there are");
+				for (Person child: children)
+				{
+					if (!child.name.equals(v))
+						System.out.println(child.name + " sibling of " + v + ". Born on " + child.DOB);
+				}
+
+			}
+			else if (victim.mother != null)
+			{
+				String name = victim.mother.name;
+				Person mother = people.get(name);
+				List<Person> children = mother.children;
+				System.out.println("Siblings of " + v + ". If none found, none there are");
+				for (Person child: children)
+				{
+					if (!child.name.equals(v))
+						System.out.println(child.name + " sibling of " + v + ". Born on " + child.DOB);
+				}
+			}
+		}
+		else
+		{
+			System.out.println("Doesn't have siblings, the blessed soul");
 		}
 	}
 }
